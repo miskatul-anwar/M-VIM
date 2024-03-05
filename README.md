eb3bb534671af54fd74fd23808060c21eedc95d4
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-05 17:11:33 +0600

    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # M-VIM
+
 A Config For VIM fans
+![image](images/mvim.png)
+
+## Installation 🚀
+
+```bash
+git clone https://github.com/miskatul-anwar/MVIM.git
+chmod +x ./install.sh
+./install.sh
+```
+
+## Dependencies 💻
+
+- git
+- vim
+- vim-plug
---
+++ install.sh
@@ -0,0 +1,21 @@
+echo "Are you sure?"
+read -r -p "Are you sure? [y/N] " response
+case "$response" in
+[yY][eE][sS] | [yY])
+	echo "Yes"
+	;;
+*)
+	echo "No"
+	exit 1
+	;;
+esac
+
+echo "Installing VIM plug..."
+printf "\n"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp sources/.vimrc ~
+cd ..
+rm -rf M-VIM
+echo "You can now run vim and enjoy M-VIM!"
