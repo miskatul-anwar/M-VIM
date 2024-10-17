b213ba2cd94ea60f3c0cf8ab18bee33c37498648
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-10-17 21:36:40 +0600

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   .install.sh

--- .install.sh
+++ .install.sh
@@ -6,7 +6,8 @@ if whiptail --title "Confirmation" --yesno "☕ Are you sure you want to install
 
 	echo "Installing M-VIM plug..."
 	printf "\n"
-
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	# Copy vimrc and clean up M-VIM
 	cp sources/.vimrc ~
 	cd ..
